7242943d5aa56148bf90f9d994e1402631b9aea3
JoaoPaulo-Web <joaopaulocoimbra2013@gmail.com>
2023-02-09 16:31:37 -0300

    Add Condicionais

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -13,3 +13,13 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png"
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector(".container h1").innerHTML = "Player 1 Wins!"
+}else if (randomNumber2 > randomNumber1){
+    document.querySelector(".container h1").innerHTML = "Player 2 Wins!"
+}else{
+    document.querySelector(".container h1").innerHTML = "It's a Draw!"
+}
